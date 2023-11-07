ee9e9ef083f44fb37bfda12a315faf28169ef209
Iv3k <126830768+Iv3k@users.noreply.github.com>
2023-11-07 12:21:39 +0100

    Cover of the book is returned from URL of API

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const apiUrl = "https://covers.openlibrary.org/b/isbn/";
+const sizeOfImg = "-S.jpg";
 
 app.use(express.static('public'));
 
@@ -23,9 +25,7 @@ app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT name, surname, title, about_book, rating, key FROM author JOIN book ON book.author_id = author.id");
 
-    const img = await axios.get("https://bored-api.appbrewery.com/random");
-
-    result.rows.forEach(row => {
+    result.rows.forEach(async row => {
     const authorName = row.name;
     const authorSurname = row.surname;
     const bookTitle = row.title;
@@ -33,15 +33,18 @@ app.get("/", async (req, res) => {
     const bookRating = row.rating;
     const bookKey = row.key;
 
+    const response = await axios.get("https://covers.openlibrary.org/b/isbn/" + bookKey + "-S.jpg");
+    
     res.render("index.ejs", {
         bookTitle: bookTitle,
         bookAuthor: authorName + " " + authorSurname,
         bookRating: bookRating,
-        bookText: bookDescription
+        bookText: bookDescription,
+        bookImage: apiUrl + bookKey + sizeOfImg
     });
 
     // Do something with the variables
-    console.log(`ISBN : ${bookKey}`);
+    console.log(`ISBN : ${apiUrl + bookKey + sizeOfImg}`);
   });
   } 
   catch (err) {
